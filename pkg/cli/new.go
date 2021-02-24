bfd72792c44a4f10188f7618bd62ca6f48837acd
Svyatoslav Dumchenko <r3nic1e@me.com>
2021-02-24 21:07:03 +0300

    update version

--- pkg/cli/new.go
+++ pkg/cli/new.go
@@ -8,7 +8,7 @@ import (
 
 func New() *helmwave.Config {
 	return &helmwave.Config{
-		Version: "0.8.2",
+		Version: "0.8.3",
 		Helm:    cli.New(),
 		Logger:  &helmwave.Log{},
 		Kubedog: &kubedog.Config{},
