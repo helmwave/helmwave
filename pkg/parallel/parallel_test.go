ad0e94a12fd45d381efc485e9ab2af58da942637
Svyatoslav Dumchenko <r3nic1e@me.com>
2021-04-18 20:44:49 +0300

    fix parallel race and add tests

---
+++ pkg/parallel/parallel_test.go
@@ -0,0 +1,44 @@
+package parallel
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/stretchr/testify/suite"
+)
+
+type ParallelTestSuite struct {
+	suite.Suite
+	wg *WaitGroup
+}
+
+func (s *ParallelTestSuite) SetupTest() {
+	s.wg = NewWaitGroup()
+}
+
+func (s *ParallelTestSuite) TestErrors() {
+	ch := s.wg.ErrChan()
+	s.Require().NotNil(ch)
+
+	err := errors.New("blabla")
+	s.wg.Add(1)
+	go func(wg *WaitGroup, err error) {
+		wg.ErrChan() <- err
+		wg.Done()
+	}(s.wg, err)
+
+	e := s.wg.Wait()
+	s.Require().Error(e)
+	s.Require().IsType(&multierror.Error{}, e)
+	s.Equal(err, e.(*multierror.Error).Unwrap())
+}
+
+func (s *ParallelTestSuite) TestWait() {}
+func (s *ParallelTestSuite) TestAdd()  {}
+func (s *ParallelTestSuite) TestDone() {}
+
+func TestPlanTestSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(ParallelTestSuite))
+}
