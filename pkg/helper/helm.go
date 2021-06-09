12749f09725969992550ab1cf2f0cd4c57db543b
zhilyaev <zhilyaev.dmitriy@gmail.com>
2021-06-09 19:08:11 +0300

    helm set env

--- pkg/helper/helm.go
+++ pkg/helper/helm.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"helm.sh/helm/v3/pkg/action"
 	helm "helm.sh/helm/v3/pkg/cli"
 	"os"
@@ -16,4 +17,17 @@ func ActionCfg(ns string, settings *helm.EnvSettings) (*action.Configuration, er
 
 	err := cfg.Init(settings.RESTClientGetter(), ns, helmDriver, log.Debugf)
 	return cfg, err
+}
+
+func setNS(ns string) (*helm.EnvSettings, error) {
+	env := helm.New()
+	fs := &pflag.FlagSet{}
+	env.AddFlags(fs)
+	flag := fs.Lookup("namespace")
+	err := flag.Value.Set(ns)
+	if err != nil {
+		return nil, err
+	}
+
+	return env, nil
 }
