4c4b3a33569057dc3b8e6a8c2a871b9bb8a6f852
zhilyaev <zhilyaev.dmitriy@gmail.com>
2021-07-13 02:38:26 +0300

    repo is parallel

---
+++ pkg/repo/config.go
@@ -0,0 +1,13 @@
+package repo
+
+import (
+	"errors"
+	"helm.sh/helm/v3/pkg/repo"
+)
+
+type Config struct {
+	repo.Entry `yaml:",inline"`
+	Force      bool
+}
+
+var ErrNotFound = errors.New("repository not found")
