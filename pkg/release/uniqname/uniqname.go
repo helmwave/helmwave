adda3bd3e8160e950911dfed1880f949fa641f93
zhilyaev <zhilyaev.dmitriy@gmail.com>
2021-11-13 01:03:37 +0300

    validate && uniqname test

--- pkg/release/uniqname/uniqname.go
+++ pkg/release/uniqname/uniqname.go
@@ -1,5 +1,12 @@
 package uniqname
 
+import (
+	"regexp"
+	"strings"
+)
+
+const Separator = "@"
+
 type UniqName string
 
 func Contains(t UniqName, a []UniqName) bool {
@@ -10,4 +17,25 @@ func Contains(t UniqName, a []UniqName) bool {
 	}
 
 	return false
+}
+
+func (n UniqName) In(a []UniqName) bool {
+	for _, v := range a {
+		if v == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (n UniqName) Validate() bool {
+	s := string(n)
+	if len(strings.Split(s, Separator)) != 2 {
+		return false
+	}
+
+	r := regexp.MustCompile("[a-z0-9]([-a-z0-9]*[a-z0-9])?" + Separator + "[a-z0-9]([-a-z0-9]*[a-z0-9])?")
+
+	return r.MatchString(s)
 }
---
+++ pkg/release/uniqname/validate_test.go
@@ -0,0 +1,34 @@
+package uniqname
+
+import "testing"
+
+func TestUniqname(t *testing.T) {
+	good := []string{
+		"my@test",
+		"my-release@test-1",
+	}
+
+	for _, s := range good {
+		if !UniqName(s).Validate() {
+			t.Error("false positive: " + s)
+		}
+	}
+
+	bad := []string{
+		"my-release",
+		"my",
+		"my@",
+		"my@-",
+		"my@ ",
+		"@name",
+		"@",
+		"@-",
+		"-@-",
+	}
+
+	for _, s := range bad {
+		if UniqName(s).Validate() {
+			t.Error("false negative: " + s)
+		}
+	}
+}
