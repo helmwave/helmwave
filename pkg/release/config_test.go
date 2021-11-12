bf33f46513ed15a50acb7238e17766eaacfa8146
zhilyaev <zhilyaev.dmitriy@gmail.com>
2021-11-13 02:34:15 +0300

    game with codecov

---
+++ pkg/release/config_test.go
@@ -0,0 +1,19 @@
+package release
+
+import "testing"
+
+func TestConfigUniq(t *testing.T) {
+	r := &Config{
+		Name:      "redis",
+		Namespace: "test",
+	}
+
+	if r.Uniq() != r.uniqName {
+		t.Error("method uniq() doesnt work")
+	}
+
+	if r.Uniq().Validate() {
+		t.Error("problem with validate uniqname")
+	}
+
+}
