35080f1a2090430da56d4aaff2dbe608a108b90e
Svyatoslav Dumchenko <zcbr95+github@gmail.com>
2021-12-13 19:21:00 +0300

    fix data race in pkg/plan/diff.go

--- pkg/plan/diff.go
+++ pkg/plan/diff.go
@@ -3,6 +3,7 @@ package plan
 import (
 	"errors"
 	"os"
+	"sync"
 
 	"github.com/databus23/helm-diff/diff"
 	"github.com/databus23/helm-diff/manifest"
@@ -95,18 +96,24 @@ func (p *Plan) GetLive() (found map[uniqname.UniqName]*live.Release, notFound []
 	wg.Add(len(p.body.Releases))
 
 	found = make(map[uniqname.UniqName]*live.Release)
+	mu := &sync.Mutex{}
 
 	for i := range p.body.Releases {
-		go func(wg *parallel.WaitGroup, rel *release.Config) {
+		go func(wg *parallel.WaitGroup, mu *sync.Mutex, rel *release.Config) {
 			defer wg.Done()
+
 			r, err := rel.Get()
+
+			mu.Lock()
+			defer mu.Unlock()
+
 			if err != nil {
 				log.Warnf("I cant get release from k8s: %v", err)
 				notFound = append(notFound, rel.Uniq())
 			} else {
 				found[rel.Uniq()] = r
 			}
-		}(wg, p.body.Releases[i])
+		}(wg, mu, p.body.Releases[i])
 	}
 
 	if err := wg.Wait(); err != nil {
