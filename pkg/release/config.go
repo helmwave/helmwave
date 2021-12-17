8e1520120614c765c7174414d098208317de5020
Svyatoslav Dumchenko <zcbr95+github@gmail.com>
2021-12-17 18:38:16 +0300

    Update config.go

--- pkg/release/config.go
+++ pkg/release/config.go
@@ -44,7 +44,6 @@ type config struct {
 	ReuseValues              bool
 }
 
-//nolint:ireturn // we need to return interface to implement interface LUL
 func (rel *config) DryRun(b bool) Config {
 	rel.dryRun = b
 	return rel
