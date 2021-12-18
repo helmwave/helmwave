56600042c2ec11938f6b4525a953f79d8a27de7f
Svyatoslav Dumchenko <zcbr95+github@gmail.com>
2021-12-18 08:11:25 +0300

    Join all tests together in one action

--- pkg/plan/export_test.go
+++ pkg/plan/export_test.go
@@ -1,19 +1,115 @@
-//go:build ignore || unit
-
 package plan
 
 import (
+	"path/filepath"
 	"testing"
 
+	"github.com/helmwave/helmwave/pkg/release"
+	"github.com/helmwave/helmwave/pkg/release/uniqname"
+	"github.com/helmwave/helmwave/pkg/template"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	helmRelease "helm.sh/helm/v3/pkg/release"
 )
+
+type mockReleaseConfig struct {
+	mock.Mock
+
+	NameF   string
+	ValuesF []release.ValuesReference
+}
+
+func (r *mockReleaseConfig) Uniq() uniqname.UniqName {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) HandleDependencies(_ []release.Config) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Sync() (*helmRelease.Release, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) NotifySuccess() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) NotifyFailed() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) DryRun(_ bool) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) ChartDepsUpd() error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) In(a []release.Config) bool {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) BuildValues(dir string, gomplate *template.GomplateConfig) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Uninstall() (*helmRelease.UninstallReleaseResponse, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Get() (*helmRelease.Release, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) List() (*helmRelease.Release, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Rollback() error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Status() (*helmRelease.Release, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Name() string {
+	return r.NameF
+}
+
+func (r *mockReleaseConfig) Namespace() string {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Chart() release.Chart {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) DependsOn() []string {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Tags() []string {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Repo() string {
+	panic("not implemented") // TODO: Implement
+}
+
+func (r *mockReleaseConfig) Values() []release.ValuesReference {
+	return r.ValuesF
+}
 
 type ExportTestSuite struct {
 	suite.Suite
 }
 
 func (s *ExportTestSuite) TestValuesEmpty() {
-	p := New(Dir)
+	tmpDir := s.T().TempDir()
+	p := New(filepath.Join(tmpDir, Dir))
 
 	p.body = &planBody{
 		Releases:     releaseConfigs{},
@@ -24,20 +120,17 @@ func (s *ExportTestSuite) TestValuesEmpty() {
 	s.Require().NoError(err)
 }
 
-/*
-// TODO: fix release.Config usage
 func (s *ExportTestSuite) TestValuesOneRelease() {
-	p := New(Dir)
+	tmpDir := s.T().TempDir()
+	p := New(filepath.Join(tmpDir, Dir))
 
 	p.body = &planBody{
 		Releases: releaseConfigs{
-			&release.Config{
-				Name:   "bitnami",
-				Values: []release.ValuesReference{},
+			&mockReleaseConfig{
+				NameF: "bitnami",
 			},
-			&release.Config{
-				Name:   "redis",
-				Values: []release.ValuesReference{},
+			&mockReleaseConfig{
+				NameF: "redis",
 			},
 		},
 	}
@@ -45,7 +138,6 @@ func (s *ExportTestSuite) TestValuesOneRelease() {
 	err := p.exportValues()
 	s.Require().NoError(err)
 }
-*/
 
 func TestExportTestSuite(t *testing.T) {
 	t.Parallel()
