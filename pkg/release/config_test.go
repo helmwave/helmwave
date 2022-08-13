293572a11eee8842a827f5cde4c4bf32df2767ed
Svyatoslav Dumchenko <zcbr95+github@gmail.com>
2022-08-13 10:46:19 +0300

    feat(tests): add test for DependsOn

--- pkg/release/config_test.go
+++ pkg/release/config_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/helmwave/helmwave/pkg/release"
+	"github.com/helmwave/helmwave/pkg/release/uniqname"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -17,6 +18,20 @@ func (s *ConfigTestSuite) TestConfigUniq() {
 	r.NamespaceF = "test"
 
 	s.Require().NoError(r.Uniq().Validate())
+}
+
+func (s *ConfigTestSuite) TestDependsOn() {
+	r := release.NewConfig()
+
+	r.NamespaceF = "testns"
+	r.DependsOnF = []string{"bla", "blabla@testns", "blablabla@testtestns"}
+
+	expected := []uniqname.UniqName{
+		uniqname.UniqName("bla@testns"),
+		uniqname.UniqName("blabla@testns"),
+		uniqname.UniqName("blablabla@testtestns"),
+	}
+	s.Require().ElementsMatch(r.DependsOn(), expected)
 }
 
 func TestConfigTestSuite(t *testing.T) {
