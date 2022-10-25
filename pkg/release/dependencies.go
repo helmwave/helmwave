0b660bff7691e1e6229f74a5e8e27579384ab181
Svyatoslav Dumchenko <zcbr95+github@gmail.com>
2022-10-25 09:16:36 +0300

    fix: update unmarshaler interface after rebase

--- pkg/release/dependencies.go
+++ pkg/release/dependencies.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/helmwave/helmwave/pkg/release/uniqname"
-	"gopkg.in/yaml.v3"
 )
 
 // ErrSkipValues is returned when values cannot be used and are skipped.
@@ -13,26 +12,17 @@ var ErrMissingDependency = errors.New("dependency is missing")
 
 // DependsOnReference is used to store release dependencies.
 type DependsOnReference struct {
-	Name     string `yaml:"name" json:"name"`
-	Optional bool   `yaml:"optional" json:"optional"`
+	Name     string `json:"name"`
+	Optional bool   `json:"optional"`
 }
 
-// UnmarshalYAML is used to implement Unmarshaler interface of gopkg.in/yaml.v3.
-func (d *DependsOnReference) UnmarshalYAML(node *yaml.Node) error {
-	type raw DependsOnReference
-	var err error
-	switch node.Kind {
-	// single value or reference to another value
-	case yaml.ScalarNode, yaml.AliasNode:
-		err = node.Decode(&d.Name)
-	case yaml.MappingNode:
-		err = node.Decode((*raw)(d))
-	default:
-		err = fmt.Errorf("unknown format")
-	}
-
-	if err != nil {
-		return fmt.Errorf("failed to decode depends_on reference %q from YAML: %w", node.Value, err)
+// UnmarshalYAML is used to implement InterfaceUnmarshaler interface of github.com/goccy/go-yaml.
+func (d *DependsOnReference) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if err := unmarshal(&d.Name); err != nil {
+		type raw DependsOnReference
+		if err := unmarshal((*raw)(d)); err != nil {
+			return fmt.Errorf("failed to decode depends_on reference from YAML: %w", err)
+		}
 	}
 
 	return nil
