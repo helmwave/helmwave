bff49711de3b6d30c78e26bb206960a0f1ec6c05
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2023-09-11 00:07:24 +0400

    feat!: drop support for old annotations

--- pkg/helper/const.go
+++ pkg/helper/const.go
@@ -3,6 +3,4 @@ package helper
 const (
 	// RootAnnoName is prefix for all kubedog annotations.
 	RootAnnoName = "helmwave.app/"
-	// OldRootAnnoName is previous prefix for all kubedog annotations.
-	OldRootAnnoName = "helmwave.dev/"
 )
