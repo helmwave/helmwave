768286847bdb7ba0073a1579d5a29255d4471861
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2023-10-11 21:52:10 +0530

    Adds option for plain-http fixes test case

--- pkg/release/chart.go
+++ pkg/release/chart.go
@@ -31,6 +31,7 @@ type Chart struct {
 	Password              string `yaml:"password" json:"password" jsonschema:"description=Chart repository password"`
 	Version               string `yaml:"version" json:"version" jsonschema:"description=Chart version"`
 	InsecureSkipTLSverify bool   `yaml:"insecure" json:"insecure" jsonschema:"description=Connect to server with an insecure way by skipping certificate verification"`
+	PlainHTTP             bool   `yaml:"plain_http" json:"plain_http" jsonschema:"description=Connect to server with plain http and not https"`
 	Verify                bool   `yaml:"verify" json:"verify" jsonschema:"description=Verify the provenance of the chart before using it"`
 	PassCredentialsAll    bool   `yaml:"pass_credentials" json:"pass_credentials" jsonschema:"description=Pass credentials to all domains"`
 	SkipDependencyUpdate  bool   `yaml:"skip_dependency_update" json:"skip_dependency_update" jsonschema:"description=Skip updating and downloading dependencies,default=false"`
@@ -44,6 +45,7 @@ func (c *Chart) CopyOptions(cpo *action.ChartPathOptions) {
 	cpo.CertFile = c.CertFile
 	cpo.KeyFile = c.KeyFile
 	cpo.InsecureSkipTLSverify = c.InsecureSkipTLSverify
+	cpo.PlainHTTP = c.PlainHTTP
 	cpo.Keyring = c.Keyring
 	cpo.Password = c.Password
 	cpo.PassCredentialsAll = c.PassCredentialsAll
