771048166371326480af6e46833967b32429e570
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2023-10-12 00:45:16 +0530

    Adds tests for reverse method

--- pkg/release/dependency/graph_test.go
+++ pkg/release/dependency/graph_test.go
@@ -120,6 +120,86 @@ func (s *GraphTestSuite) TestCycles() {
 	s.Require().Error(graph.Build())
 }
 
+// 1 -> 3 -> 4
+// 2 /
+// Reversed
+// 4 -> 3 -> 1
+// --------> 2
+func (s *GraphTestSuite) TestReverse() {
+	graph := dependency.NewGraph[string, string]()
+
+	s.Require().NoError(graph.NewNode("1", "1"))
+	s.Require().NoError(graph.NewNode("2", "2"))
+	s.Require().NoError(graph.NewNode("3", "3"))
+	s.Require().NoError(graph.NewNode("4", "4"))
+
+	graph.AddDependency("3", "1")
+	graph.AddDependency("3", "2")
+	graph.AddDependency("4", "3")
+
+	s.Require().NoError(graph.Build())
+	graph, err := graph.Reverse()
+	s.Require().Nil(err)
+
+	ch := graph.Run()
+
+	s.Require().NotNil(ch)
+	s.Require().Eventually(func() bool { return len(ch) == 1 }, time.Second, time.Millisecond)
+	s.Require().Never(func() bool { return len(ch) > 1 }, time.Second, time.Millisecond)
+	d := <-ch
+	s.Require().Equal("4", d.Data)
+	d.SetSucceeded()
+
+	s.Require().Eventually(func() bool { return len(ch) == 1 }, time.Second, time.Millisecond)
+	s.Require().Never(func() bool { return len(ch) > 1 }, time.Second, time.Millisecond)
+	d = <-ch
+	s.Require().Equal("3", d.Data)
+	d.SetSucceeded()
+
+	s.Require().Eventually(func() bool { return len(ch) == 2 }, time.Second, time.Millisecond)
+	s.Require().Never(func() bool { return len(ch) > 2 }, time.Second, time.Millisecond)
+	d1, d2 := <-ch, <-ch
+	s.Require().ElementsMatch([]string{"1", "2"}, []string{d1.Data, d2.Data})
+	d1.SetSucceeded()
+	d2.SetSucceeded()
+
+	s.Require().Never(func() bool { return len(ch) > 0 }, time.Second, time.Millisecond)
+}
+
+func (s *GraphTestSuite) TestReverseFailedDependencies() {
+	graph := dependency.NewGraph[string, string]()
+
+	s.Require().NoError(graph.NewNode("1", "1"))
+	s.Require().NoError(graph.NewNode("2", "2"))
+	s.Require().NoError(graph.NewNode("3", "3"))
+	s.Require().NoError(graph.NewNode("4", "4"))
+
+	graph.AddDependency("3", "1")
+	graph.AddDependency("3", "2")
+	graph.AddDependency("4", "3")
+
+	s.Require().NoError(graph.Build())
+	graph, err := graph.Reverse()
+	s.Require().Nil(err)
+
+	ch := graph.Run()
+
+	s.Require().NotNil(ch)
+	s.Require().Eventually(func() bool { return len(ch) == 1 }, time.Second, time.Millisecond)
+	s.Require().Never(func() bool { return len(ch) > 1 }, time.Second, time.Millisecond)
+	d := <-ch
+	s.Require().Equal("4", d.Data)
+	d.SetSucceeded()
+
+	s.Require().Eventually(func() bool { return len(ch) == 1 }, time.Second, time.Millisecond)
+	s.Require().Never(func() bool { return len(ch) > 1 }, time.Second, time.Millisecond)
+	d = <-ch
+	s.Require().Equal("3", d.Data)
+	d.SetFailed()
+
+	s.Require().Never(func() bool { return len(ch) > 0 }, time.Second, time.Millisecond)
+}
+
 func TestGraphTestSuite(t *testing.T) {
 	t.Parallel()
 	suite.Run(t, new(GraphTestSuite))
