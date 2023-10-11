dc33b0dc5e6ed25b2154c73a95a4a9ea4f7db132
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2023-10-12 01:20:51 +0530

    Uses success in both failed and success cases

--- pkg/plan/down.go
+++ pkg/plan/down.go
@@ -47,12 +47,11 @@ func (p *Plan) Down(ctx context.Context) (err error) {
 			defer wg.Done()
 			rel := node.Data
 			_, err := rel.Uninstall(ctx)
+			node.SetSucceeded()
 			if err != nil {
-				node.SetFailed()
 				log.Errorf("❌ %s: %v", rel.Uniq(), err)
 				wg.ErrChan() <- err
 			} else {
-				node.SetSucceeded()
 				log.Infof("✅ %s uninstalled!", rel.Uniq())
 			}
 		}(ctx, wg, node)
