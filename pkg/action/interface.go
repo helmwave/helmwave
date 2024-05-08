9c6418f00ca71c3a8a10b6358c72e976cf31cab6
Sviatoslav Dumchenko <zcbr95+github@gmail.com>
2024-05-08 10:24:26 +0400

    chore: refactor context funcs into separate package

--- pkg/action/interface.go
+++ pkg/action/interface.go
@@ -3,7 +3,7 @@ package action
 import (
 	"context"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/helmwave/helmwave/pkg/clictx"
 	"github.com/urfave/cli/v2"
 )
 
@@ -17,21 +17,8 @@ type Action interface {
 // toCtx is a wrapper for urfave v2.
 func toCtx(a func(context.Context) error) func(c *cli.Context) error {
 	return func(c *cli.Context) error {
-		ctx := getContextWithFlags(c)
+		ctx := clictx.CLIContextToContext(c)
 
 		return a(ctx)
 	}
-}
-
-func getContextWithFlags(c *cli.Context) context.Context {
-	ctx := c.Context
-	for _, flagName := range c.FlagNames() {
-		g := c.Value(flagName)
-		log.WithField("name", flagName).WithField("value", g).Trace("adding flag to action context.Context")
-		ctx = context.WithValue(ctx, flagName, g) //nolint:staticcheck // weird issue, we won't have any collisions with strings
-	}
-
-	ctx = context.WithValue(ctx, "cli", c) //nolint:staticcheck // same
-
-	return ctx
 }
